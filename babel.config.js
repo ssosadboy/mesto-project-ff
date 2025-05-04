eb0dcfcd8b15062ed8a452cf77b7ba7979f16e3c
Vkirill <k.viktor0v@yandex.ru>
2025-05-04 21:48:44 +0300

    set

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+// Используем синтаксис ES-модулей
+export default {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        edge: '17',
+        ie: '11',
+        firefox: '50',
+        chrome: '64',
+        safari: '11.1',
+      },
+      useBuiltIns: 'entry',
+      corejs: '3',
+    }],
+  ],
+};
